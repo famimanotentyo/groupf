77105830bf567b6e983ad8838059786ab44b7fea
famimanotentyo <komonofami@gmail.com>
2025-11-06 14:04:32 +0900

    test

--- groupf/tasks/views.py
+++ groupf/tasks/views.py
@@ -8,3 +8,24 @@ def top_page(request):
     # 将来的にここでデータベースからタスク情報を取得し、context経由でテンプレートに渡します。
     context = {}
     return render(request, 'tasks/index.html', context)
+
+def task_assign(request):
+    """
+    タスク割り当てページを表示するビュー
+    """
+    context = {}
+    return render(request, 'tasks/task_assign.html', context)
+
+def task_list(request):
+    """
+    タスク一覧ページを表示するビュー
+    """
+    context = {}
+    return render(request, 'tasks/task_list.html', context)
+
+def task_redister(request):
+    """
+    タスク登録ページを表示するビュー
+    """
+    context = {}
+    return render(request, 'tasks/task_redister.html', context)
