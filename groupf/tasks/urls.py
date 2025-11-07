73073a8ec247e6ab5670b914c471d6ba439fa83c
Sato-1125 <gurami20160101@gmail.com>
2025-11-07 10:21:44 +0900

    あ

--- groupf/tasks/urls.py
+++ groupf/tasks/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('task_assign/', views.task_assign, name='task_assign'),
     path('task_board/', views.task_board, name='task_board'),
     path('task_register/', views.task_register, name='task_register'),
-    path('management/', views.management_support_page, name='management_support_page')
+    path('management/', views.management_support_page, name='management_support_page'),
+    path('task-assign/', views.task_assign_page, name='task_assign_page'),
+    path('task-board/', views.task_board_page, name='task_board_page'),
+    path('task-register/', views.task_register_page, name='task_register_page'),
     
 ]
